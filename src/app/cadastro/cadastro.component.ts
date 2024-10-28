dfba13a7b6617363b2ca881a46ff96b5866db673
Pauloglhr <pauloglhr@outlook.com>
2024-10-28 20:51:27 -0300

    adicionado validacoes iniciais no campo de nome

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
@@ -13,7 +13,11 @@ export class CadastroComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  cadastrar(){
-      console.log('Formulário enviado');
+  cadastrar(form : NgForm){
+      if(form.valid){
+        this.router.navigate(['./sucesso'])
+      } else {
+        alert('Formulário invalido')
+      }
   }
 }
